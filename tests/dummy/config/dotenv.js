81447b4a1aa56c84b055733ffa8a40c8f73bf15c
sandydoo <sandermel94@gmail.com>
2018-02-20 19:21:16 +0300

    Deploy to gh-pages

--- tests/dummy/config/dotenv.js
+++ tests/dummy/config/dotenv.js
@@ -1,7 +1,8 @@
 /* eslint-env node */
-module.exports = function(/*env*/) {
+module.exports = function(env) {
   return {
     clientAllowedKeys: ['GOOGLE_MAPS_API_KEY'],
-    failOnMissingKey: true
+    failOnMissingKey: true,
+    path: `./.env-${env}`
   };
 };
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -58,7 +58,6 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
   }
 
   return ENV;
