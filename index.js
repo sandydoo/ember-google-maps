ce8f367fdd86be6ff25ac75c09d088bcd349098e
sandydoo <sandermel94@gmail.com>
2018-02-26 03:39:12 +0300

    Add options to treeshake components

--- index.js
+++ index.js
@@ -1,8 +1,57 @@
 /* eslint-env node */
 'use strict';
 
+const Funnel = require('broccoli-funnel');
+const path = require('path');
+
+function intersection(a, b) {
+  const intersection = new Set();
+  for (let e of b) {
+    if (a.has(e)) {
+      intersection.add(e);
+    }
+  }
+  return intersection;
+}
+
+function difference(a, b) {
+  const difference = new Set(a);
+  for (let e of b) {
+    difference.delete(e);
+  }
+  return difference;
+}
+
+const dependencies = {
+  'circle': ['marker'],
+  'info-window': ['detect-render']
+};
+
 module.exports = {
   name: 'ember-google-maps',
+
+  included() {
+    this._super.included.apply(this, arguments);
+
+    const app = this._findHost();
+
+    const config = app.options['ember-google-maps'] || {};
+    this.whitelist = this.generateWhitelist(config);
+    this.blacklist = this.generateBlacklist(config);
+
+    // If a whitelist is used, ensure that we include the base map component.
+    if (this.whitelist.length) {
+      this.whitelist.push('g-map', 'canvas', 'base');
+      this.whitelist.forEach((w) => {
+        const deps = dependencies[w];
+        if (deps) {
+          this.whitelist = this.whitelist.concat(deps);
+        }
+      });
+    }
+
+    this.noFiltersDefined = (!this.whitelist || this.whitelist.length === 0) && (!this.blacklist || this.blacklist.length === 0);
+  },
 
   config(env, config) {
     let src = '//maps.googleapis.com/maps/api/js';
@@ -51,5 +100,86 @@ module.exports = {
     config['ember-google-maps'] = mapConfig;
 
     return config;
+  },
+
+  treeForAddon() {
+    const tree = this._super.treeForAddon.apply(this, arguments);
+    return this.filterComponents(tree);
+  },
+
+  treeForAddonTemplates() {
+    const tree = this._super.treeForAddonTemplates.apply(this, arguments);
+    return this.filterComponents(tree);
+  },
+
+  filterComponents(tree) {
+    const whitelist = this.whitelist;
+    const blacklist = this.blacklist;
+
+    if (this.noFiltersDefined) {
+      return tree;
+    }
+
+    return new Funnel(tree, {
+      exclude: [(name) => this.excludeComponent(name, whitelist, blacklist)]
+    });
+  },
+
+  excludeComponent(name, whitelist, blacklist) {
+    const regex = /components\//;
+
+    const isComponent = regex.test(name);
+    if (!isComponent) {
+      return false;
+    }
+
+    let baseName = path.basename(name);
+    baseName = baseName.split('.').shift();
+
+    let isWhiteListed = whitelist.indexOf(baseName) !== -1;
+    let isBlackListed = blacklist.indexOf(baseName) !== -1;
+
+    if (whitelist.length === 0 && blacklist.length === 0) {
+      return false;
+    }
+
+    // Include if both white- and blacklisted
+    if (isWhiteListed && isBlackListed) {
+      return false;
+    }
+
+    // Only whitelisted
+    if (whitelist.length && blacklist.length === 0) {
+      return !isWhiteListed;
+    }
+
+    // Only blacklisted
+    if (blacklist.length && whitelist.length === 0) {
+      return isBlackListed;
+    }
+
+    return !isWhiteListed || isBlackListed;
+  },
+
+  generateWhitelist(config) {
+    const only = new Set(config.only || []);
+    const except = new Set(config.except || []);
+
+    if (except && except.length) {
+      return difference(only, except);
+    }
+
+    return Array.from(only);
+  },
+
+  generateBlacklist(config) {
+    const only = new Set(config.only || []);
+    const except = new Set(config.except || []);
+
+    if (only && only.length) {
+      return intersection(except, only);
+    }
+
+    return Array.from(except);
   }
 };
