7a7fa372052a25d8e9e9e5266e2d47ab41ead51b
sandydoo <sandermel94@gmail.com>
2018-03-14 11:50:22 +0000

    Remove jQuery from dummy app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,10 @@ const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   let app = new EmberAddon(defaults, {
+    vendorFiles: {
+      'jquery.js': null
+    },
+
     snippetPaths: ['tests/dummy/app/templates/snippets'],
     includeHighlightStyle: false,
 
