cbad2f1447cd32f5082f5d81271f0706b102ebd1
sandydoo <sandermel94@gmail.com>
2018-04-02 19:04:43 +0300

    Use color input in circle docs

--- tests/dummy/app/controllers/docs/circles.js
+++ tests/dummy/app/controllers/docs/circles.js
@@ -3,7 +3,7 @@ import CommonMapData from '../../mixins/common-map-data';
 import { computed } from '@ember/object';
 
 export default DocsController.extend(CommonMapData, {
-  fillColor: 'green',
+  fillColor: '#00F900',
 
   radius: computed({
     get: () => 1000,
