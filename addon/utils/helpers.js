137717c571c867a5a6ec3952bb955acc6e6290a7
sandydoo <sandermel94@gmail.com>
2018-05-08 22:06:56 +0300

    Reuse computed property with promise support

--- addon/utils/helpers.js
+++ addon/utils/helpers.js
@@ -1,4 +1,8 @@
 import { computed, getProperties } from '@ember/object';
+import ObjectProxy from '@ember/object/proxy';
+import PromiseProxyMixin from '@ember/object/promise-proxy-mixin';
+
+let ObjectPromiseProxy = ObjectProxy.extend(PromiseProxyMixin);
 
 let position = computed('lat', 'lng', function() {
   const { lat, lng } = getProperties(this, 'lat', 'lng');
@@ -7,4 +11,13 @@ let position = computed('lat', 'lng', function() {
   }
 });
 
-export { position };
+function computedPromise(...args) {
+  let func = args.pop();
+  return computed(...args, function() {
+    return ObjectPromiseProxy.create({
+      promise: func.apply(this)
+    });
+  });
+}
+
+export { computedPromise, position };
