fdbad8f2a2f2e0edcfe4fc61a66f4f4f439f86b3
sandydoo <sandermel94@gmail.com>
2018-07-13 02:27:58 +0300

    Remove wrapping div from addon-facotry

--- addon/components/-private-api/addon-factory.js
+++ addon/components/-private-api/addon-factory.js
@@ -3,6 +3,7 @@ import layout from '../../templates/components/-private-api/addon-factory';
 
 export default Component.extend({
   layout,
+  tagName: '',
 
   init() {
     this._super(...arguments);
