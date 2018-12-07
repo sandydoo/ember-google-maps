d8c2eb90725d8c623e80474c7d3b2c4c7bd6183c
sandydoo <sandermel94@gmail.com>
2018-12-07 16:09:43 +0300

    Disable template linting

--- .template-lintrc.js
+++ .template-lintrc.js
@@ -1,5 +1,14 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+  extends: 'recommended',
+
+  // Disable this for now. Investigate what the specific rules
+  // are and decide whether to adopt. Seems to demand some
+  // really crap styling for block components...
+  ignore: [
+    'addon/templates/components/**',
+    'tests/**',
+    'dummy/templates/**'
+  ],
 };
