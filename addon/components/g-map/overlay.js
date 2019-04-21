974f15ca45ca4ddf1f870c0eb6411915075105d8
sandydoo <sandermel94@gmail.com>
2019-04-21 04:32:30 +0300

    Update Overlay
    Improve the flow of initial setup of the Overlay component.

--- addon/components/g-map/overlay.js
+++ addon/components/g-map/overlay.js
@@ -1,12 +1,17 @@
-import MapComponent from './map-component';
+import { default as MapComponent, MapComponentLifecycleEnum } from './map-component';
 import layout from '../../templates/components/g-map/overlay';
+import { addEventListeners } from '../../utils/options-and-events';
 import { position } from '../../utils/helpers';
 import { computed, get, set } from '@ember/object';
-import { reads } from '@ember/object/computed';
-import { join, next, schedule } from '@ember/runloop';
+import { bind, once, scheduleOnce } from '@ember/runloop';
 import { guidFor } from '@ember/object/internals';
 import { htmlSafe } from '@ember/string';
-import { Promise } from 'rsvp';
+import { assert } from '@ember/debug';
+import { defer, resolve } from 'rsvp';
+
+
+const { READY } = MapComponentLifecycleEnum;
+
 
 /**
  * A wrapper for the google.maps.Overlay class.
@@ -26,7 +31,6 @@ export default MapComponent.extend({
   paneName: 'overlayMouseTarget',
 
   _targetPane: null,
-  _eventTarget: reads('content'),
 
   innerContainerStyle: htmlSafe('transform: translateX(-50%) translateY(-100%);'),
 
@@ -35,41 +39,65 @@ export default MapComponent.extend({
   }),
 
   _addComponent() {
+    let isFinishedDrawing = defer();
     let Overlay = new google.maps.OverlayView();
 
-    return new Promise((resolve) => {
-      Overlay.onAdd = () => join(this, 'add');
-      Overlay.draw = () => schedule('render', () => {
-        if (this.isDestroyed) { return; }
-        this._initialDraw();
-        resolve();
-      });
-      Overlay.onRemove = () => join(this, 'destroy');
+    function onAdd() {
+      if (this.isDestroyed) { return; }
 
-      set(this, 'mapComponent', Overlay);
-      next(() => {
-        this.mapComponent.setMap(get(this, 'map'));
-      });
-    });
+      let panes = this.mapComponent.getPanes();
+      set(this, '_targetPane', panes[this.paneName]);
+    }
+
+    function initialDraw() {
+      if (this.isDestroyed) { return; }
+
+      let contentId = get(this, '_contentId');
+      let content = this.fetchOverlayContent(contentId);
+
+      assert('No content', Boolean(content));
+
+      set(this, 'content', content);
+
+      (this.mapComponent.draw = () => scheduleOnce('render', this, 'draw'))();
+
+      isFinishedDrawing.resolve(this.mapComponent);
+    }
+
+    Overlay.onAdd = () => once(this, onAdd);
+
+    Overlay.draw = () => scheduleOnce('afterRender', this, initialDraw);
+
+    Overlay.onRemove = bind(this, 'destroy');
+
+    set(this, 'mapComponent', Overlay);
+
+    this.mapComponent.setMap(this.map);
+
+    return isFinishedDrawing.promise;
   },
 
-  _initialDraw() {
-    this.fetchOverlayContent();
-    this.mapComponent.draw = () => this.draw();
-    this._updateComponent();
+  _didAddComponent(_, options, events) {
+    let payload = {
+      map: this.map,
+      publicAPI: this.publicAPI,
+    };
+
+    addEventListeners(this.content, events, payload)
+      .forEach(({ name, remove }) => this._eventListeners.set(name, remove));
+
+    return resolve();
   },
 
   _updateComponent() {
-    this.mapComponent.draw();
-  },
-
-  add() {
-    if (this.isDestroyed) { return; }
-    let panes = this.mapComponent.getPanes();
-    set(this, '_targetPane', get(panes, this.paneName));
+    if (this.mapComponentLifecycle === READY) {
+      this.mapComponent.draw();
+    }
   },
 
   draw() {
+    if (this.isDestroyed) { return; }
+
     let overlayProjection = this.mapComponent.getProjection();
     let position = get(this, 'position');
     let point = overlayProjection.fromLatLngToDivPixel(position);
@@ -83,12 +111,7 @@ export default MapComponent.extend({
     `;
   },
 
-  fetchOverlayContent() {
-    let element = document.getElementById(get(this, '_contentId'));
-    set(this, 'content', element);
-  },
-
-  getPosition() {
-    return get(this, 'position');
+  fetchOverlayContent(id) {
+    return document.getElementById(id);
   }
 });
