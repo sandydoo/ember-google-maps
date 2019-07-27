32bfe07bedea2289cc96a0178d10fe77d90dda7a
sandydoo <sandermel94@gmail.com>
2019-07-27 23:50:52 +0200

    Add Ember@2.8lts to tests

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,6 +7,18 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
+        name: 'ember-lts-2.18',
+        env: {
+          EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
+        },
+        npm: {
+          devDependencies: {
+            '@ember/jquery': '^0.5.1',
+            'ember-source': '~2.18.0'
+          }
+        }
+      },
+      {
         name: 'ember-lts-3.4',
         npm: {
           devDependencies: {
