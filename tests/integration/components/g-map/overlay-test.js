19cca87cbb2bf2cbe32c932b213c62b1c4d3f80d
sandydoo <7572407+sandydoo@users.noreply.github.com>
2020-05-05 19:50:20 +0300

    Add overlay performance test

--- tests/integration/components/g-map/overlay-test.js
+++ tests/integration/components/g-map/overlay-test.js
@@ -4,6 +4,36 @@ import { setupMapTest } from 'ember-google-maps/test-support';
 import { setupLocations } from 'dummy/tests/helpers/locations';
 import { find, render, waitFor } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
+import { set } from '@ember/object';
+import { later } from '@ember/runloop';
+
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function coinToss() {
+  return Math.random() >= 0.5 ? true : false;
+}
+
+function perturbLocations(times) {
+  for (let i = 1; i <= times; i++) {
+    later(() => {
+      set(this, 'locations', this.originalLocations.filter(coinToss));
+    }, 50 * i);
+  }
+}
+
+function generateLocations(google, center) {
+  let { lat, lng } = center,
+      origin = new google.maps.LatLng(lat, lng);
+
+  return Array(42).fill().map((_e, i) => {
+    let heading = randomInt(1, 360),
+        distance = randomInt(100, 5000),
+        n = google.maps.geometry.spherical.computeOffset(origin, distance, heading);
+    return { id: i, lat: n.lat(), lng: n.lng() };
+  });
+}
 
 module('Integration | Component | g map/overlay', function(hooks) {
   setupRenderingTest(hooks);
@@ -28,5 +58,28 @@ module('Integration | Component | g map/overlay', function(hooks) {
     assert.ok(overlay, 'overlay rendered');
 
     assert.ok(mapDiv.contains(overlay), 'overlay is child of map node');
+  });
+
+  test('it survives a performance test without errors', async function(assert) {
+    assert.expect(0);
+
+    let center = { lat: this.lat, lng: this.lng },
+        googleMapsApi = this.owner.lookup('service:google-maps-api'),
+        google = await googleMapsApi.google;
+
+    this.originalLocations = await generateLocations(google, center);
+    this.locations = this.originalLocations;
+
+    await render(hbs`
+      {{#g-map lat=lat lng=lng zoom=12 as |g|}}
+        {{#each locations as |loc|}}
+          {{#g.overlay lat=loc.lat lng=loc.lng}}
+            <div>Test</div>
+          {{/g.overlay}}
+        {{/each}}
+      {{/g-map}}
+    `);
+
+    await perturbLocations.call(this, 30);
   });
 });
