eabe401922739284fad41cc37e2218f37b7222a2
sandydoo <7572407+sandydoo@users.noreply.github.com>
2020-06-01 06:29:04 +0300

    Reproduce ember-cli-babel bug

--- addon/utils/helpers.js
+++ addon/utils/helpers.js
@@ -9,6 +9,15 @@ let position = computed('lat', 'lng', function() {
   return (lat && lng) ? new google.maps.LatLng(lat, lng) : undefined;
 });
 
+// This one also doesn't transpile properly.
+function position2() {
+  return computed('lat', 'lng', function() {
+    const { lat, lng } = getProperties(this, 'lat', 'lng');
+    return (lat && lng) ? new google.maps.LatLng(lat, lng) : undefined;
+  });
+}
+
+// The ...args here plays a role and forces babel to add a `var _Ember` in scope.
 function computedPromise(...args) {
   let func = args.pop();
   return computed(...args, function() {
--- tests/dummy/config/targets.js
+++ tests/dummy/config/targets.js
@@ -9,9 +9,9 @@ const browsers = [
 const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
 
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
+// if (isCI || isProduction) {
+browsers.push('ie 11');
+// }
 
 module.exports = {
   browsers
