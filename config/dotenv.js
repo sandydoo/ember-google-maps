9df2bdd0580eacf8bdca4075a58ad75c7d2c7c34
sandydoo <hey@sandydoo.me>
2020-10-16 23:21:37 +0300

    Disable dotenv in CI
    We use Travis's env variables anyways. This should suppress
    some of the extraneous error messages.

--- config/dotenv.js
+++ config/dotenv.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
 module.exports = function(env) {
+  let isCI = Boolean(process.env.CI);
+
   return {
+    enabled: !isCI, // disable for CI
     clientAllowedKeys: ['GOOGLE_MAPS_API_KEY'],
     failOnMissingKey: false,
     path: `./.env.${env}`
