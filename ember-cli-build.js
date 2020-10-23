d2c2c3efe00f1827d59598ff2ed4441db35f05f6
sandydoo <hey@sandydoo.me>
2020-10-24 00:36:32 +0300

    Add optional chaining support to the docs app

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,12 @@ module.exports = function(defaults) {
   let options = {
     snippetPaths: ['tests/dummy/app/templates/snippets'],
 
+    babel: {
+      plugins: [
+        '@babel/plugin-proposal-optional-chaining'
+      ]
+    },
+
     'ember-cli-babel': {
       includePolyfill: true
     },
