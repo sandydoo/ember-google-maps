1ec384ab25f43d2ccfb37a131a547e8eefbfcaa9
sandydoo <hey@sandydoo.me>
2021-04-02 02:11:38 +0300

    Improve code snippet

--- tests/dummy/code-snippets/safely-accessing-google.js
+++ tests/dummy/code-snippets/safely-accessing-google.js
@@ -1,11 +1,11 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
-import { reads } from '@ember/object/computed';
 
 export default class extends Controller {
   @service
   googleMapsApi;
 
-  @reads('googleMapsApi.google')
-  google;
+  get google() {
+    return this.googleMapsApi.google;
+  }
 }
