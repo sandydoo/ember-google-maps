ecffe1e27ad5e8ae29b9721cae4b50a8c29a5abd
sandydoo <hey@sandydoo.me>
2021-04-04 19:47:32 +0300

    Hide implementation details of action tracker

--- addon-test-support/setup-action-tracking.js
+++ addon-test-support/setup-action-tracking.js
@@ -1,20 +1,18 @@
 import { Promise, resolve } from 'rsvp';
 
 export class ActionTracker {
-  constructor() {
-    this.tracked = new Map();
-    this.watcher = new Map();
-  }
+  #tracked = new Map();
+  #watcher = new Map();
 
   seen(name, options = {}) {
     if (!name) {
       return;
     }
 
-    let target = this.tracked.get(name);
+    let target = this.#tracked.get(name);
 
     if (target?.count > target?.lastSeen) {
-      this.tracked.set(name, {
+      this.#tracked.set(name, {
         ...target,
         lastSeen: target.count,
       });
@@ -24,7 +22,7 @@ export class ActionTracker {
     let { timeout = 10000 } = options;
 
     return new Promise((resolve, reject) => {
-      this.watcher.set(name, resolve);
+      this.#watcher.set(name, resolve);
 
       setTimeout(() => {
         reject(new Error(`Timed out waiting for ${name}.`));
@@ -33,20 +31,20 @@ export class ActionTracker {
   }
 
   notify(name) {
-    let resolve = this.watcher.get(name);
-    this.watcher.delete(name);
+    let resolve = this.#watcher.get(name);
+    this.#watcher.delete(name);
     return resolve?.();
   }
 
   track(name) {
-    let { count = 0, lastSeen = 0 } = this.tracked.get(name) ?? {};
+    let { count = 0, lastSeen = 0 } = this.#tracked.get(name) ?? {};
 
-    this.tracked.set(name, {
+    this.#tracked.set(name, {
       count: ++count,
       lastSeen,
     });
 
-    if (this.watcher.has(name)) {
+    if (this.#watcher.has(name)) {
       this.notify(name);
     }
   }
