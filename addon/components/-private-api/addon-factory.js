0a7c9b22ee4172ce941ad4871130dd341fa2921d
sandydoo <hey@sandydoo.me>
2021-04-10 17:27:01 +0300

    Remove addon factory component

--- addon/components/-private-api/addon-factory.js
+++
@@ -1,7 +0,0 @@
-import Component from '@ember/component';
-import layout from '../../templates/components/-private-api/addon-factory';
-
-export default Component.extend({
-  layout,
-  tagName: '',
-});
