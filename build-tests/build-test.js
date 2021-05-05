484d880c6f626e49fa2045a0da0075b39fde5fce
sandydoo <hey@sandydoo.me>
2021-05-05 20:37:48 +0300

    Fix build tests
    - Cap the Ember version we use for `v3.x` to `v3.22.x`
    - Don’t swallow errors if something fails

--- build-tests/build-test.js
+++ build-tests/build-test.js
@@ -3,7 +3,6 @@
 const path = require('path');
 const chalk = require('chalk');
 const AddonTestApp = require('ember-cli-addon-tests').AddonTestApp;
-const getURLFor = require('ember-source-channel-url');
 
 // Set up Google Maps key
 const dotenvConf = require('../config/dotenv')('development');
@@ -11,7 +10,6 @@ require('dotenv').config(dotenvConf);
 
 const log = (msg) => console.log(chalk.green(msg));
 const debug = (msg) => console.log(chalk.gray(msg));
-const warn = (msg) => console.log(chalk.red(msg));
 
 function prepareOptions(options = {}) {
   return Object.entries(options)
@@ -24,14 +22,11 @@ async function createApp(appName) {
 
   let app = new AddonTestApp();
 
-  let url = await getURLFor('release');
-
-  await app.create(
-    appName,
-    { emberVersion: url,
-      fixturesPath: 'build-tests/fixtures/',
-    }
-  );
+  await app.create(appName, {
+    emberVersion: '~3.22.0',
+    fixturesPath: 'build-tests/fixtures/',
+    log: debug,
+  });
 
   return app;
 }
@@ -41,7 +36,7 @@ async function buildApp(app, options = {}) {
 
   let buildOptions = prepareOptions(options);
 
-  await app.run('ember', 'build', ...buildOptions, { log: debug });
+  await app.runEmberCommand('build', ...buildOptions, { log: debug });
 
   return app;
 }
@@ -55,21 +50,25 @@ async function testCI(app, options = {}) {
   debug(`Serving from: ${distPath}`);
 
   let testOptions = prepareOptions(options);
-  await app.run('ember', 'test', ...testOptions, '--path', distPath, '--test-page', testPage, { log: console.log });
+  await app.runEmberCommand(
+    'test',
+    ...testOptions,
+    '--path',
+    distPath,
+    '--test-page',
+    testPage,
+    { log: console.log }
+  );
 
   return app;
 }
 
 async function runTests() {
-  try {
-    let app = await createApp('treeshaking-test');
+  let app = await createApp('treeshaking-test');
 
-    await buildApp(app, { environment: 'test' });
-    await testCI(app);
-
-  } catch (error) {
-    warn(`Something went wrong. ${error.message}`);
-  }
+  await buildApp(app, { environment: 'test' });
+  
+  await testCI(app);
 }
 
 runTests();
