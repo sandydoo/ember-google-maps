3e1ee3f1765916d54a145c935989f167d2c9c2fc
sandydoo <hey@sandydoo.me>
2021-05-29 19:05:58 +0300

    Fix modifier manager capabilities deprecation
    - Replace `@ember/render-modifiers` with a simple `did-insert` modifier.
    - Use the updated `ember-modifier` with support for the new modifier
      capabilities.

---
+++ app/modifiers/g-map/did-insert.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-google-maps/modifiers/g-map/did-insert';
---
+++ tests/integration/modifiers/g-map/did-insert-test.js
@@ -0,0 +1,15 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import { hbs } from 'ember-cli-htmlbars';
+
+module('Integration | Modifier | g-map/did-insert', function(hooks) {
+  setupRenderingTest(hooks);
+
+  // Replace this with your real tests.
+  test('it renders', async function(assert) {
+    await render(hbs`<div {{g-map/did-insert}}></div>`);
+
+    assert.ok(true);
+  });
+});
