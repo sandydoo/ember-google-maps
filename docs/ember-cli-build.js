a11563f9960a530c2f7455a2feef3d831be8eabd
Sander <hey@sandydoo.me>
2023-05-14 02:25:40 +0400

    docs: enable static embroider build

--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -74,7 +74,18 @@ module.exports = function(defaults) {
   // along with the exports of each module as its value.
 
   const { Webpack } = require('@embroider/webpack');
-  const compiledApp = require('@embroider/compat').compatBuild(app, Webpack);
+  const compiledApp = require('@embroider/compat').compatBuild(app, Webpack, {
+    skipBabel: [
+      {
+        package: 'qunit',
+      },
+    ],
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticComponents: true,
+  });
 
   return require('prember').prerender(app, compiledApp);
 };
