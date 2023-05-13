86e28048e02460e19f8d2b24c2d5a97034528ae2
Sander <hey@sandydoo.me>
2023-05-14 02:09:22 +0400

    docs: Upgrade Ember v4.4.0...v4.12.1

--- docs/ember-cli-build.js
+++ docs/ember-cli-build.js
@@ -2,8 +2,8 @@
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
-module.exports = function (defaults) {
-  let app = new EmberApp(defaults, {
+module.exports = function(defaults) {
+  const app = new EmberApp(defaults, {
     snippetPaths: ['code-snippets'],
 
     babel: {
