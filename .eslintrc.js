c958b7dda9c6cfacd0d2481652c2c844a765e2aa
Sander <hey@sandydoo.me>
2023-12-26 03:51:06 +0400

    Disable qunit/require-assert lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,6 +65,10 @@ module.exports = {
       // test files
       files: ['tests/**/*-test.{js,ts}'],
       extends: ['plugin:qunit/recommended'],
+      rules: {
+        // Disable assert.expect errors
+        'qunit/require-expect': 'off',
+      },
     },
   ],
 };
