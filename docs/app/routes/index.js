19c4159fa14fbebe6068c645d5c701080c0ce672
Sander <hey@sandydoo.me>
2023-12-26 03:14:40 +0400

    docs: switch to transitionTo from RouterService

--- docs/app/routes/index.js
+++ docs/app/routes/index.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class extends Route {
+  @service router;
+
   beforeModel() {
-    this.transitionTo('docs.getting-started');
+    this.router.transitionTo('docs.getting-started');
   }
 }
