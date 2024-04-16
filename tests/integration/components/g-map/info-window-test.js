c5f818920e5028e606415f443a4bd2ec490d53cb
colenso <colenso.castellino@gmail.com>
2024-04-16 14:54:55 +0530

    chore: use marker instead of advancedMarker in info-window-test

--- tests/integration/components/g-map/info-window-test.js
+++ tests/integration/components/g-map/info-window-test.js
@@ -111,11 +111,11 @@ module('Integration | Component | g-map/info-window', function (hooks) {
   test('it attaches an info window to a marker', async function (assert) {
     await render(hbs`
       <GMap @lat={{this.lat}} @lng={{this.lng}} @zoom={{6}} as |g|>
-        <g.advancedMarker @lat={{55}} @lng={{2}} as |m|>
+        <g.marker @lat={{55}} @lng={{2}} as |m|>
           <m.infoWindow
             @isOpen={{true}}
             @content="Testing info windows attached to markers" />
-        </g.advancedMarker>
+        </g.marker>
       </GMap>
     `);
 
@@ -132,18 +132,19 @@ module('Integration | Component | g-map/info-window', function (hooks) {
 
     await render(hbs`
       <GMap @lat={{this.lat}} @lng={{this.lng}} @zoom={{6}} as |g|>
-        <g.advancedMarker @lat={{55}} @lng={{2}} as |m|>
+        <g.marker @lat={{55}} @lng={{2}} as |m|>
           <m.infoWindow
             @isOpen={{this.isOpen}}>
             <div id="info-window-test">
               An info window attached to a marker!
             </div>
           </m.infoWindow>
-        </g.advancedMarker>
+        </g.marker>
       </GMap>
   `);
 
     let infoWindow = await this.getFirstInfoWindow();
+    await waitUntil(() => isVisible(infoWindow));
 
     assert.ok(find('#info-window-test'));
     assert.ok(isVisible(infoWindow));
@@ -166,18 +167,19 @@ module('Integration | Component | g-map/info-window', function (hooks) {
 
     await render(hbs`
       <GMap @lat={{this.lat}} @lng={{this.lng}} @zoom={{6}} as |g|>
-        <g.advancedMarker @lat={{55}} @lng={{2}} as |m|>
+        <g.marker @lat={{55}} @lng={{2}} as |m|>
           <m.infoWindow
             @isOpen={{this.isOpen}}
             @onCloseclick={{this.closeInfoWindow}}
             @content="<div id='info-window-test'>Testing the close button!</div>"/>
-        </g.advancedMarker>
+        </g.marker>
       </GMap>
     `);
 
     let infoWindow = await this.getFirstInfoWindow();
 
     assert.ok(find('#info-window-test'));
+    await waitUntil(() => isVisible(infoWindow));
     assert.ok(isVisible(infoWindow), 'info window is visible');
 
     trigger(infoWindow, 'closeclick');
