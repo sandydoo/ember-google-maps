6d0a69dba481ac1a258c1edb54f451432b2584f3
colenso <colenso.castellino@gmail.com>
2024-04-17 11:07:06 +0530

    chore: switch to loading the google maps api dynamically

--- addon/services/google-maps-api.js
+++ addon/services/google-maps-api.js
@@ -18,17 +18,8 @@ export default class GoogleMapsApiService extends Service {
     return this.google.then((google) => new google.maps.DirectionsService());
   }
 
-  /**
-   * By default, this returns the Google Maps URL created at build time. You can
-   * use this hook to build the URL at runtime instead.
-   *
-   * Optionally, you can return a promise that resolves with the URL. This
-   * allows you to use external data when building the URL. For example, you
-   * could fetch the database record for the current user for localisation
-   * purposes.
-   */
-  buildGoogleMapsUrl(config) {
-    return config['src'];
+  overrideConfig(config) {
+    return config;
   }
 
   /**
@@ -56,34 +47,60 @@ export default class GoogleMapsApiService extends Service {
 
     let config = this._getConfig();
 
-    return resolve(config)
-      .then(this.buildGoogleMapsUrl)
-      .then(this._loadAndInitApi);
+    return resolve(config).then(this.overrideConfig).then(this._loadAndInitApi);
   }
 
-  _loadAndInitApi(src) {
+  _loadAndInitApi(config) {
+    let { channel, key, language, libraries, region, version, mapIds } = config;
+
     assert(
-      `
-ember-google-maps: You tried to load the Google Maps API, but the source URL was empty. \
-Perhaps you forgot to specify the API key? \
+      `need to specify the API key? \
 Learn more: https://ember-google-maps.sandydoo.me/docs/getting-started`,
-      src,
+      key,
     );
 
     return new Promise((resolve, reject) => {
-      window.initGoogleMap = bind(() => {
+      window.initGoogleMap = bind(async () => {
+        if (libraries && libraries.length > 0) {
+          for (let library of libraries) {
+            await google.maps.importLibrary(library);
+          }
+        }
         resolve(window.google);
       });
 
+      let versionString = version ? `version: "${version}",` : '';
+      let channelString = channel ? `channel: "${channel}",` : '';
+      let regionString = region ? `region: "${region}",` : '';
+      let languageString = language ? `language: "${language}",` : '';
+      let mapIdsString = mapIds ? `mapIds: ${mapIds},` : '';
+
       let s = document.createElement('script');
       s.type = 'text/javascript';
-      s.async = true;
+      s.textContent =
+        `(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=\`https://maps.\${c}apis.com/maps/api/js?\`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
+          key: "${key}",` +
+        versionString +
+        `
+            ` +
+        channelString +
+        `
+            ` +
+        regionString +
+        `
+            ` +
+        languageString +
+        `
+            ` +
+        mapIdsString +
+        `
+          // Add other bootstrap parameters as needed, using camel case.
+        });
+        initGoogleMap();
+      `;
       s.onerror = (error) => reject(error);
       // Insert into DOM to avoid CORS problems
       document.body.appendChild(s);
-
-      // Load map
-      s.src = `${src}&callback=initGoogleMap`;
     });
   }
 }
