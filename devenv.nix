e789d185582d2625d61b10b849ab4a583cdaaa0a
Sander <hey@sandydoo.me>
2024-12-03 21:17:05 +0400

    dev: add devenv

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{ ... }:
+
+{
+  languages.javascript = {
+    enable = true;
+    pnpm = {
+      enable = true;
+      install.enable = true;
+    };
+  };
+
+  languages.typescript.enable = true;
+}
